2e0ebd155de549f062a01261951dc09aca02ac7c
SAR0S <weorbit79@gmail.com>
2021-01-07 21:50:12 +0900

    Create print_for_loop.py

---
+++ basic/print_for_loop.py
@@ -0,0 +1,19 @@
+for x in [1,2,3]: #리스트의 요소들을 하나씩 x에 대입한다.
+    print(x)
+
+print()
+
+for y in (1, 2, 3): #튜플
+    print(y)
+
+print()
+
+dic = {'key':1, 'key2':2, 'key3':3} #딕셔너리
+for z in dic:
+    print(z)
+    print(dic[z])
+
+print()
+
+for a in 'abcdefg': #문자열
+    print(a)
