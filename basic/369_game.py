c2a38c0a72bb1f68e98a686f5fa3056a08b37176
SAR0S <weorbit79@gmail.com>
2021-01-11 17:38:29 +0900

    Create 369_game.py

---
+++ basic/369_game.py
@@ -0,0 +1,11 @@
+def solve(n):
+    i = 1
+    while i <= n:
+        if i % 3 == 0:
+            print("X")
+        else:
+            print(i)
+        i = i + 1
+
+if __name__ == "__main__":
+    solve(30)
