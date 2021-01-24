a76695b510179c0e7edd8bb284516c4781dc1208
SAR0S <weorbit79@gmail.com>
2021-01-24 20:34:14 +0900

    Create fill_array02.py

---
+++ basic/fill_array02.py
@@ -0,0 +1,15 @@
+a = [0] * 101
+def calc_array(n):
+    a[1] = 1
+
+    i = 2
+    while i <= n:
+        a[i] = a[i//2] + 1
+        i = i + 1
+
+if __name__ == "__main__":
+    calc_array(100)
+    i = 0
+    while i <= 100:
+        print(a[i], end=" ")
+        i = i + 1
