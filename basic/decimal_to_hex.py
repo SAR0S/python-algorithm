d616726cfa0b96766cf083600973fd5a26ea47cb
SAR0S <weorbit79@gmail.com>
2021-02-01 20:15:13 +0900

    Create decimal_to_hex.py

---
+++ basic/decimal_to_hex.py
@@ -0,0 +1,10 @@
+d = "0123456789ABCDEFGHIJ"
+def f(n, k):
+    if n > 0:
+        f(n//k, k)
+        print("%c"%d[n%k], end='')
+
+if __name__ == "__main__":
+    n = int(input("변환할 10진수 입력 : "))
+    k = int(input("변환 진수 입력 : "))
+    f(n, k)
