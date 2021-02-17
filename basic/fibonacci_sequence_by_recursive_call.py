d5113c82b81484c4b7da799c42edd45354ade2f4
SAR0S <weorbit79@gmail.com>
2021-02-17 20:55:59 +0900

    Create fibonacci_sequence_by_recursive_call.py

---
+++ basic/fibonacci_sequence_by_recursive_call.py
@@ -0,0 +1,10 @@
+def fib(n):
+    if n <= 2:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+if __name__ == "__main__":
+    
+    for i in range(31):
+        print("피보나치 %2d = %10d"%(i, fib(i)))
